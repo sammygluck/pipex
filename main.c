a1d060fc2b8955e2e6b54ec8c84dacab679bdb48
Sammy <infogls@yahoo.com>
2023-05-07 23:17:38 -0400

    still testing

--- main.c
+++ main.c
@@ -1,19 +1,17 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
-int main(int argc, char **argv)
-{
-    char **args = {"/bin/ls", "-l", NULL};
-    char **env = {NULL};
+int main(int argc, char *argv[]) {
+    char *args[] = { "/bin/ls", "-la", NULL };
+    char *env[] = { NULL };
     int ret;
 
     ret = execve(args[0], args, env);
-    if (ret == -1)
-    {
+    if (ret == -1) {
         perror("execve");
-        return (1);
+        return 1;
     }
-    return (0);
+
+    return 0;
 }
+
