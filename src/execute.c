085ac4f77a89ca8c540d280483ec5a2dea9d73b6
Sammy Gluck <infogls@yahoo.com>
2023-05-14 15:01:19 -0700

    change to execute

--- src/execute.c
+++ src/execute.c
@@ -6,12 +6,9 @@ int execute(char *command, char **arguments, char **paths)
     int ret;
 
     i = 0;
-    while (paths[i])
-    {
-        ret = execve(command, arguments, paths[i]);
-        if (ret == -1)
-            return (-1);
-        i++;
-    }
+    ret = execve(command, arguments, paths);
+    if (ret == -1)
+        return (-1);
+    i++;
     return (-1);
 }
