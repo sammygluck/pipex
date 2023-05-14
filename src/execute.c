15366955c4ae94674db9b1f75d1fe4361a9f20c7
Sammy Gluck <infogls@yahoo.com>
2023-05-14 15:16:52 -0700

    another change

--- src/execute.c
+++ src/execute.c
@@ -4,11 +4,17 @@ int execute(char *command, char **arguments, char **paths)
 {
     int i;
     int ret;
+    char *full_path;
 
     i = 0;
-    ret = execve(command, arguments, paths);
-    if (ret == -1)
-        return (-1);
-    i++;
+    while (paths[i])
+    {
+        full_path = ft_strjoin(paths[i], command);
+        ret = execve(full_path, arguments, NULL);
+        if (ret == -1)
+                return (-1);
+        i++;
+    }
+    
     return (-1);
 }
