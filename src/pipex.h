d85ce77cfa40e3541fc85953b4b37b6bc335b6ce
Sammy Gluck <infogls@yahoo.com>
2023-05-21 17:30:42 -0700

    fixed error_exit 17:30 21/05 Home

--- src/pipex.h
+++ src/pipex.h
@@ -25,7 +25,7 @@ int execute(char *command, char **arguments, char **paths);
 t_pipex *var_init(char **argv, char **envp);
 char  **parsed_envp(char *envp[]);
 char **arg_parser(char *arguments);
-void    error_exit(t_pipex variables, char *msg);
+void    error_exit(t_pipex *variables, char *msg);
 int     fork_handler(t_pipex *variables);
 void free_string_array(char **array);
 
