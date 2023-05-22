c3bab228203af2f5cdf7220a9495a1b6a3aa1b29
Sammy Gluck <infogls@yahoo.com>
2023-05-21 19:21:07 -0700

    debug

--- src/6_execute.c
+++ src/6_execute.c
@@ -1,5 +1,32 @@
 #include "pipex.h"
 
+// int execute(char *command, char **arguments, char **paths)
+// {
+//     int i;
+//     int ret;
+//     char *full_path;
+
+//     i = 0;
+//     while (paths[i])
+//     {
+//         full_path = ft_strjoin(paths[i], command);
+//         if (!full_path)
+//             return (-1);
+//         if (access(full_path, X_OK) == 0)
+//         {
+//             ret = execve(full_path, arguments, NULL);
+//             free (full_path);
+//             if (ret == -1)
+//                 return (-1);
+//         }
+//         else
+//             free(full_path);
+//         i++;
+//     }
+//     ft_putstr_fd("pipex: command not found: ", 2);
+//     ft_putendl_fd(command, 2);
+//     return (-1);
+// }
 int execute(char *command, char **arguments, char **paths)
 {
     int i;
@@ -23,7 +50,6 @@ int execute(char *command, char **arguments, char **paths)
             free(full_path);
         i++;
     }
-    ft_putstr_fd("pipex: command not found: ", 2);
-    ft_putendl_fd(command, 2);
+    fprintf(stderr, "execute: command not found: %s\n", command); // Print error message if command not found
     return (-1);
 }
