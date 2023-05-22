94c299dcf3890dbe773323217f39f83b4ebf36a0
Sammy Gluck <infogls@yahoo.com>
2023-05-21 17:35:38 -0700

    error_exit for main removed

--- src/1_main.c
+++ src/1_main.c
@@ -6,7 +6,10 @@ int main(int argc, char **argv, char **envp)
     int ret;
 
     if (argc != 5)
-        error_exit("Invalid number of arguments");
+    {
+        perror("invalid number of arguments");
+        return (1);
+    }
     variables = var_init(argv, envp);
     ret = fork_handler(variables);
     return (ret);
