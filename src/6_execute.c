049e9879719b2be787d9adec8fdcb35058cdf645
Sammy Gluck <infogls@yahoo.com>
2023-05-30 18:26:16 -0700

    this hopefully works

--- src/6_execute.c
+++ src/6_execute.c
@@ -1,135 +1,87 @@
 #include "pipex.h"
 
+//debug 1
 
-static int execute_command(char *full_path, char **arguments)
+static int try_execute(char *full_path, char **arguments)
 {
     int ret;
+    
+    ret = execve(full_path, arguments, NULL);
+    free(full_path);
+    if (ret == -1)
+        return (-1);
 
-    if (access(full_path, X_OK) == 0) 
+    return 0;
+}
+
+static int handle_file_access(char *full_path)
+{
+    int permission_issue;
+    
+    permission_issue = 0;
+    if (access(full_path, X_OK) != 0)
     {
-        ret = execve(full_path, arguments, NULL);
-        free(full_path);
-        if (ret == -1)
-            return -1;
-    } 
-    else 
-    {
-        free(full_path);
         if (errno == EACCES)
-            return -2;  // return special error code for permission denied
-        else
-            return -3;  // return special error code for command not found
+            permission_issue = 1;
+        free(full_path);
     }
+    return permission_issue;
+}
 
+static int process_single_path(char *full_path, char **arguments)
+{
+    if (access(full_path, X_OK) == 0)
+    {
+        if (try_execute(full_path, arguments) == -1)
+            return (-1);
+    }
+    else
+        return handle_file_access(full_path);
     return 0;
 }
 
 static int iterate_paths(char **paths, char *command, char **arguments)
 {
     int i;
-    int ret;
+    int permission_issue;
     char *full_path;
 
     i = 0;
+    permission_issue = 0;
     while (paths[i])
     {
         full_path = ft_strjoin(paths[i], command);
         if (!full_path)
-            return -1;
-        ret = execute_command(full_path, arguments);
-        if (ret != 0)
-            return ret;
+            return (-1);
+        permission_issue = process_single_path(full_path, arguments);  
+        if (permission_issue != 0)
+            return (permission_issue);
         i++;
     }
-    return 0;
+    return permission_issue;
 }
 
 int execute(char *command, char **arguments, char **paths)
 {
-    return iterate_paths(paths, command, arguments);
+    iterate_paths(paths, command, arguments);
+    return (-1);
 }
 
-
-
-
-
-
-//debug 1
-
-// static int try_execute(char *full_path, char **arguments)
-// {
-//     int ret;
-    
-//     ret = execve(full_path, arguments, NULL);
-//     free(full_path);
-//     if (ret == -1)
-//         return (-1);
-
-//     return 0;
-// }
-
-// static int handle_file_access(char *full_path)
-// {
-//     int permission_issue;
-    
-//     permission_issue = 0;
-//     if (access(full_path, X_OK) != 0)
-//     {
-//         if (errno == EACCES)
-//             permission_issue = 1;
-//         free(full_path);
-//     }
-//     return permission_issue;
-// }
-
-// static int process_single_path(char *full_path, char **arguments)
-// {
-//     if (access(full_path, X_OK) == 0)
-//     {
-//         if (try_execute(full_path, arguments) == -1)
-//             return (-1);
-//     }
-//     else
-//         return handle_file_access(full_path);
-//     return 0;
-// }
-
-// static int iterate_paths(char **paths, char *command, char **arguments)
-// {
-//     int i;
-//     int permission_issue;
-//     char *full_path;
-
-//     i = 0;
-//     permission_issue = 0;
-//     while (paths[i])
-//     {
-//         full_path = ft_strjoin(paths[i], command);
-//         if (!full_path)
-//             return (-1);
-//         permission_issue = process_single_path(full_path, arguments);  
-//         if (permission_issue != 0)
-//             return (permission_issue);
-//         i++;
-//     }
-//     return permission_issue;
-// }
-
 // int execute(char *command, char **arguments, char **paths)
-//{
-    // int permission_issue;
+// {
+//     // int permission_issue;
     
-    // permission_issue = 
-    //iterate_paths(paths, command, arguments);
-    // if (permission_issue == -1)
-    //     return (-1);
-    // if (permission_issue) 
-    //     ft_putstr_fd("pipex: command permission denied: ", 2);
-    // else 
-    //     ft_putstr_fd("pipex: command not found: ", 2);
-    // ft_putendl_fd(command, 2);
-    //return (-1);
-//}
+//     // permission_issue = 
+//     iterate_paths(paths, command, arguments);
+//     // if (permission_issue == -1)
+//     //     return (-1);
+//     // if (permission_issue) 
+//     //     ft_putstr_fd("pipex: command permission denied: ", 2);
+//     // else 
+//     //     ft_putstr_fd("pipex: command not found: ", 2);
+//     // ft_putendl_fd(command, 2);
+//     return (-1);
+// }
 
 //debug 1
 
--- src/7_fork_handler.c
+++ src/7_fork_handler.c
@@ -1,104 +1,25 @@
 #include "pipex.h"
 
-//debug code
-// int command_exists(char *command, char **paths)
-// {
-//     char *full_path;
-//     int i = 0;
-
-//     while (paths[i]) 
-//     {
-//         full_path = ft_strjoin(paths[i], command);
-//         if (!full_path)
-//             return (0);
-//         if (access(full_path, X_OK) == 0) {
-//             free(full_path);
-//             return (1);
-//         }
-//         else
-//             free(full_path);
-//         i++;
-//     }
-//     return (0);
-// }
-
-// static void handle_first_child(t_pipex *variables)
-// {
-//     close(variables->pipe_fd[0]);
-//     if (dup2(variables->fd, STDIN_FILENO) == -1)
-//         error_exit(variables, "dup2");
-//     if (dup2(variables->pipe_fd[1], STDOUT_FILENO) == -1)
-//         error_exit(variables, "dup2");
-//     if(command_exists(variables->args[0], variables->paths)) 
-//     {
-//         if (execute(variables->args[0], variables->args, variables->paths) == -1)
-//             error_exit(variables, args[0]);
-//     }
-//     else 
-//     {
-//         //needs to change
-//         // ft_putstr_fd("pipex: command not found: ", 2);
-//         // ft_putendl_fd(command, 2);
-//         fprintf(stderr, "Command %s does not exist.\n", variables->args[0]);
-//         exit(EXIT_FAILURE);
-//     }
-// }
-
-// static void handle_second_child(t_pipex *variables)
-// {
-//     close(variables->pipe_fd[1]);
-//     if(dup2(variables->pipe_fd[0], STDIN_FILENO) == -1)
-//         error_exit(variables, "dup2");
-//     if(dup2(variables->fd2, STDOUT_FILENO) == -1)
-//         error_exit(variables, "dup2");
-//     if(command_exists(variables->args2[0], variables->paths)) {
-//         if(execute(variables->args2[0], variables->args2, variables->paths) == -1)
-//             error_exit(variables, "execute");    }
-//     else 
-//     {
-//         //fprintf can't be used
-//         //ft_putstr_fd("pipex: command not found: ", 2);
-//         //ft_putendl_fd(command, 2);
-//         fprintf(stderr, "Command %s does not exist.\n", variables->args2[0]);
-//         exit(EXIT_FAILURE);
-//     }
-// }
-
-//end debug
-
-#include <stdio.h>
-
 static void handle_first_child(t_pipex *variables)
 {
-    int ret;
-
     close(variables->pipe_fd[0]);
     if (dup2(variables->fd, STDIN_FILENO) == -1)
         error_exit(variables, "dup2");
     if (dup2(variables->pipe_fd[1], STDOUT_FILENO) == -1)
         error_exit(variables, "dup2");
-    ret = execute(variables->args[0], variables->args, variables->paths) == -1;
-    fprintf(stderr, "%s", strerror(ret));
-    error_exit(variables, variables->args[0]); 
-
-    // if (execute(variables->args[0], variables->args, variables->paths) == -1)
-    //     error_exit(variables, variables->args[0]); 
+    if (execute(variables->args[0], variables->args, variables->paths) == -1)
+        error_exit(variables, variables->args[0]); 
 }
 
 static void handle_second_child(t_pipex *variables)
 {
-    int ret;
-    
     close(variables->pipe_fd[1]);
     if(dup2(variables->pipe_fd[0], STDIN_FILENO) == -1)
         error_exit(variables, "dup2");
     if(dup2(variables->fd2, STDOUT_FILENO) == -1)
         error_exit(variables, "dup2");
-    ret = execute(variables->args2[0], variables->args2, variables->paths) == -1;
-    fprintf(stderr, "%s", strerror(ret));
-    error_exit(variables, variables->args2[0]); 
-    // if(execute(variables->args2[0], variables->args2, variables->paths) == -1)
-    //     error_exit(variables, variables->args2[0]); 
+    if(execute(variables->args2[0], variables->args2, variables->paths) == -1)
+        error_exit(variables, variables->args2[0]); 
 }
 
 static void handle_parent(t_pipex *variables)
