33ed0bc0feefa115cb372cc5e6281871ccacec3e
Sammy Gluck <infogls@yahoo.com>
2023-07-02 13:15:21 +0200

    one by one

--- src/6_fork_handler.c
+++ src/6_fork_handler.c
@@ -36,7 +36,7 @@ static void	handle_second_child(t_pipex *variables)
 	if (dup2(variables->fd2, STDOUT_FILENO) == -1)
 		error_exit(variables, "dup2", 0);
 	ret = execute(variables->args2[0], variables->args2, variables->paths); 
-	error_exit(variables, variables->args2[0], ret);
+	//error_exit(variables, variables->args2[0], ret);
 }
 
 static void	handle_parent(t_pipex *variables)
